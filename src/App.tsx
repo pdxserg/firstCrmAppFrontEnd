a55815f8202ff28c8763f24c97d5d8735b79b8c0
SergeyStarchenko <pdxserg@gmail.com>
2025-03-12 07:49:46 -0700

    change name

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit <code>src/App.tsx</code> and save to test HMR!!!!
         </p>
       </div>
 
