a48080be72ff3d92301c8e0ece80b4a18d4b39db
SergeyStarchenko <pdxserg@gmail.com>
2025-03-11 21:46:31 -0700

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+// @flow
+
+export const Header = ( ) => {
+	return (
+		<div>
+			<h2>My Cool Header</h2>
+		</div>
+	);
+};
+
+// export const Header = () => {
+// 	return (<div>
+// 			<h2>My Cool Header</h2>
+// 	</header>
+// );
+// };
